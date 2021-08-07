d035723f9e71fd6279f9b4fef1cf03eb30d0eac8
won_chan <83020544+boychan112@users.noreply.github.com>
2021-08-07 11:18:37 +0900

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -35,6 +35,23 @@ async def on_message(message):
         embed=discord.Embed(description="https://dak.gg/bser/players/kokoakim", color=0x00ff56)
         embed.set_author(name="kokoakim")
         await message.channel.send(embed=embed)
+        
+    if content.startswith("!음식"):
+        embed=discord.Embed(title=" ", description=" ", color=0x00ff56)
+        embed.set_author(name="```음식 지도```")
+        embed.set_image(url="https://cdn.discordapp.com/attachments/873384182997479457/873384193722298398/common.png")
+        await message.channel.send(embed=embed)
+
+    if content.startswith("!지도"):
+        embed=discord.Embed(title=" ", description=" ", color=0x00ff56)
+        embed.set_author(name="```지도```")
+        embed.set_image(url="https://cdn.discordapp.com/attachments/873384182997479457/873384713216225341/common.png")
+        await message.channel.send(embed=embed)
+
+    if content.startswith("!레시피"):
+        embed=discord.Embed(description="```힐링포션```\n난초 + 유리병\n난초 = 약초 + 꽃\n========================\n```구급상자```\n지혈제 + 붕대\n지혈제 = 알코올 + 붕대\n========================\n```생선튀김```\n붕어 + 뜨거운 오일\n뜨거운 오일 = 오일 + 라이터\n========================\n```감자튀김```\n감자 + 뜨거운 오일\n뜨거운 오일 = 오일 + 라이터\n========================\n```피쉬앤칩스```\n감자튀김 + 생선튀김", color=0x00ff56)
+        embed.set_author(name="kokoakim")
+        await message.channel.send(embed=embed)
 
 access_token = os.environ['BOT_TOKEN']
 client.run(access_token)
