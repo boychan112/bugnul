8ad63e6bbbeaeda48a2b7601471145b0b6b1933a
won_chan <83020544+boychan112@users.noreply.github.com>
2021-08-07 10:38:06 +0900

    Add files via upload

---
+++ Bot.py
@@ -0,0 +1,38 @@
+import discord
+import datetime
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("디스코드 봇 로그인이 완료되었습니다.")
+    print("디스코드 봇 이름 : " +client.user.name)
+    print("디스코드 봇 ID" +str(client.user.id))
+    print("디스코드봇 버전 : " + str(discord.__version__))
+    print("datetime 버전 : "+str(datetime.__version__))
+    print('------')
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("버그널 리턴"))
+
+@client.event
+async def on_message(message):
+    content = message.content
+    
+    if content.startswith("!자기소개"):
+        await message.channel.send("응애 나 아기 디코 봇")
+
+    if content.startswith("!이원찬"):
+        embed=discord.Embed(description="https://dak.gg/bser/players/plati", color=0x00ff56)
+        embed.set_author(name="plati")
+        await message.channel.send(embed=embed)
+
+    if content.startswith("!최현영"):
+        embed=discord.Embed(description="https://dak.gg/bser/players/light00", color=0x00ff56)
+        embed.set_author(name="light00")
+        await message.channel.send(embed=embed)
+
+    if content.startswith("!최승민"):
+        embed=discord.Embed(description="https://dak.gg/bser/players/kokoakim", color=0x00ff56)
+        embed.set_author(name="kokoakim")
+        await message.channel.send(embed=embed)
+
+client.run('ODcyMDc5Nzg1NDc4NzUwMjQ4.YQkplw.SWoH_BjjDjo2q0MECvBc7PVvTzc')
